124afa1ca7525cdaf7c1b55ade4d025f6374be95
Martin Rubli <martin.rubli@integra-biosciences.com>
2025-03-05 18:03:31 +0100

    _server

--- test.cpp
+++ test.cpp
@@ -7,27 +7,51 @@
 namespace
 {
 
-ssize_t custom_read(coap_session_t *session, uint8_t *data, size_t datalen)
+ssize_t custom_client_read(coap_session_t *session, uint8_t *data, size_t datalen)
 {
-	std::println("custom_read: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
+	std::println("[client] read: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
 	return 0;
 }
 
-ssize_t custom_write(coap_session_t *session, const uint8_t *data, size_t datalen)
+ssize_t custom_client_write(coap_session_t *session, const uint8_t *data, size_t datalen)
 {
-	std::println("custom_write: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
+	std::println("[client] write: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
 	return datalen;
 }
 
-void custom_establish(coap_session_t *session)
+void custom_client_establish(coap_session_t *session)
 {
-	std::println("custom_establish: session: {}", (void *)session);
+	std::println("[client] establish: session: {}", (void *)session);
 }
 
-void custom_close(coap_session_t *session)
+void custom_client_close(coap_session_t *session)
 {
-	std::println("custom_close: session: {}", (void *)session);
+	std::println("[client] close: session: {}", (void *)session);
 }
+
+
+ssize_t custom_server_read(coap_session_t *session, uint8_t *data, size_t datalen)
+{
+	std::println("[server] read: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
+	return 0;
+}
+
+ssize_t custom_server_write(coap_session_t *session, const uint8_t *data, size_t datalen)
+{
+	std::println("[server] write: session: {}, data: {}, datalen: {}", (void *)session, (void *)data, datalen);
+	return datalen;
+}
+
+void custom_server_establish(coap_session_t *session)
+{
+	std::println("[server] establish: session: {}", (void *)session);
+}
+
+void custom_server_close(coap_session_t *session)
+{
+	std::println("[server] close: session: {}", (void *)session);
+}
+
 
 int run_client()
 {
@@ -36,10 +60,10 @@ int run_client()
 	coap_context_t *ctx = coap_new_context(nullptr);
 
 	const auto custom_callbacks = coap_custom_transport_func_t{
-		.read = custom_read,
-		.write = custom_write,
-		.establish = custom_establish,
-		.close = custom_close,
+		.read =      custom_client_read,
+		.write =     custom_client_write,
+		.establish = custom_client_establish,
+		.close =     custom_client_close,
 	};
 	coap_set_custom_transport_func(ctx, &custom_callbacks);
 
@@ -63,6 +87,40 @@ int run_client()
 	coap_cleanup();
 }
 
+int run_server()
+{
+	std::println("[server] starting up");
+
+	coap_context_t *ctx = coap_new_context(nullptr);
+
+	const auto custom_callbacks = coap_custom_transport_func_t{
+		.read =      custom_server_read,
+		.write =     custom_server_write,
+		.establish = custom_server_establish,
+		.close =     custom_server_close,
+	};
+	coap_set_custom_transport_func(ctx, &custom_callbacks);
+
+	coap_endpoint_t *ep = coap_new_endpoint(ctx, nullptr, COAP_PROTO_CUSTOM);
+
+	coap_resource_t *resource = coap_resource_init(coap_make_str_const("hello"), 0);
+	coap_register_request_handler(resource, COAP_REQUEST_GET,
+		[] (auto, auto, const coap_pdu_t *request, auto, coap_pdu_t *response) {
+			coap_show_pdu(COAP_LOG_WARN, request);
+			coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+			coap_add_data(response, 5, (const uint8_t *)"world");
+			coap_show_pdu(COAP_LOG_WARN, response);
+		})
+	;
+	coap_add_resource(ctx, resource);
+
+	while (true)
+	{
+		std::println("[server] coap_io_process");
+		coap_io_process(ctx, COAP_IO_WAIT);
+	}
+}
+
 } // anonymous namespace
 
 
@@ -74,9 +132,12 @@ int main(int argc, char *argv[])
 	coap_startup();
 	coap_set_log_level(COAP_LOG_DEBUG);
 
+	auto server_thread = std::thread{ run_server };
+	std::this_thread::sleep_for(std::chrono::seconds{ 1 });
 	auto client_thread = std::thread{ run_client };
 
 	client_thread.join();
+	server_thread.join();
 
 	return 0;
 }
